3823edf81ca8e75a2d1622f59d47e8d27edc0574
Rajat22UEE <rajatnath6909@gmail.com>
2024-05-17 18:35:22 +0530

    Let start it

---
+++ README.md
@@ -0,0 +1,4 @@
+Date 17-05-2024 
+# Start the project NIT Agartala @2024
+
+Team: Sudip , Rajat , Ratnadip , Samiran
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+// main.jsx present here
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
