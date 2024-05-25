be5568eb14433fcfa554d82d7a3f2d02d2a63afc
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-05-25 18:13:04 +0530

    first push by sudip

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <img src="https://tse2.mm.bing.net/th/id/OIP.F4yP8-6cDGpvYpCffzkokwHaEK?rs=1&pid=ImgDetMain" alt="Error" />
+    </div>
+  )
+}
+
