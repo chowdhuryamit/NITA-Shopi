97b8cd61b50826886cdd2c34ace01a0c2708815d
Rajat22UEE <rajatnath6909@gmail.com>
2024-05-28 13:52:30 +0530

    Demo Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 // Here the App.jsx present
 
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Here we start the project</h1>
+      <Header/>
     </>
   )
 }
