50454244be1d63811b4efaa8d782c3f862678f69
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-05-29 22:28:55 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 // Here the App.jsx present
 
 import { useState } from 'react'
-<<<<<<< HEAD
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Components/Header'
 
-=======
 import './App.css'
-import Home from './components/Home'
->>>>>>> ce31255df3b3868ec66af0d57ba8fef80fdfffdb
+import Header from './components/Header'
+import Home from './pages/Home'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
+
       <Header/>
-=======
       <h1>Here we start the project</h1>
       <Home/>
->>>>>>> ce31255df3b3868ec66af0d57ba8fef80fdfffdb
+
     </>
   )
 }
