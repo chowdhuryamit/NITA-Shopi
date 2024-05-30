43edd066f19542df5495651a8de50f01244442b5
Rajat22UEE <rajatnath6909@gmail.com>
2024-05-30 13:52:29 +0530

    Pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import Header from './Components/Header'
-import Home from './pages/Home'
-import './App.css'
 
-function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './pages/Home';
+import Service from './pages/Service';
+import About from './pages/About';
+import Help from './pages/Help';
+
+const App = () => {
   return (
-    <>
+    <Router>
       <Header/>
-      <br />
-       <Home/> 
-    </>
-  )
-}
+      <Routes>
+        <Route exact path="/Home" element={<Home />} />
+        <Route path="/Service" element={<Service />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Help" element={<Help />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+
+
+const About = () => {
+    return (
+     <>
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold">About Page</h1>
+      </div>
+     </>
+    );
+  }
+  
+  export default About;
+  
