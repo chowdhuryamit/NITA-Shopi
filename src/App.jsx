4fb11f0d6d0722dfa327a380245f0eed33b0f0ce
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-05-30 12:08:31 +0530

    Coflict Resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import './App.css'
 import Header from './components/Header'
-import './App.css'
 import Home from './pages/Home'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
