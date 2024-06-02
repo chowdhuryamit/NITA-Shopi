f1b49ae3a9c80c8a188c106b0a99b0861f4a9bb6
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-06-02 12:46:53 +0530

    Facilities Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,7 @@ export default function Home() {
   };
 
   return (
-    <div >   
+    <div>
       {/* Left-Side */}
       <section className="bg-[#FCF8F1] bg-opacity-30 py-10 sm:py-16 lg:py-24">
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -72,9 +72,56 @@ export default function Home() {
 
       {/* Facilities */}
 
-      
-
- 
+      <div className="flex flex-col items-center text-center">
+  <div className="text-center text-4xl font-bold">
+    <h3>Our Facilities</h3>
+  </div>
+  <div className="w-60 h-1 border-b-4 border-green-400 my-3"></div>
+  <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+    <div className="flex flex-col justify-center items-center">
+      <img
+        src="https://tse3.mm.bing.net/th/id/OIP.qGa4rZh-9Zof10CcbaIAfAHaF3?rs=1&pid=ImgDetMain"
+        alt=""
+        className="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 rounded-full border-4 border-green-500"
+      />
+      <p className="font-semibold mt-4">
+        Old Products: "Find hidden treasures among gently used items!"
+      </p>
     </div>
+    <div className="flex flex-col justify-center items-center">
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/016/471/452/original/abstract-modern-ecommerce-logo-ecommerce-logo-design-shop-logo-design-template-creative-ecommerce-logo-vector.jpg"
+        alt=""
+        className="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 rounded-full border-4 border-green-500"
+      />
+      <p className="font-semibold mt-4">
+        New Products: "Discover the latest and greatest products at unbeatable prices!"
+      </p>
+    </div>
+    <div className="flex flex-col justify-center items-center">
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/000/623/004/original/auto-car-logo-template-vector-icon.jpg"
+        alt=""
+        className="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 rounded-full border-4 border-green-500"
+      />
+      <p className="font-semibold mt-4">
+        Auto Services: "Need a ride urgently? Connect with a driver instantly!"
+      </p>
+    </div>
+    <div className="flex flex-col justify-center items-center">
+      <img
+        src="https://png.pngtree.com/template/20200704/ourlarge/pngtree-restaurant-logo-design-vector-template-image_388753.jpg"
+        alt=""
+        className="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 rounded-full border-4 border-green-500"
+      />
+      <p className="font-semibold mt-4">
+        Restaurant Menus: "Satisfy your cravings with a diverse range of culinary delights!"
+      </p>
+    </div>
+  </div>
+</div>
+
+    </div>
+    
   );
 }
