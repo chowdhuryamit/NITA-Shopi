296c097cf70217293ae0481c8cadd1a3b2095c3b
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-11-30 14:20:05 +0530

    added

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,52 @@
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+
+const UserSchema= new mongoose.Schema({
+    fullname: {
+        type: String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"],
+    },
+    phonenumber:{
+        type:String,
+        required:[true,"PhoneNumber is required"],
+    }
+},{timestamps:true})
+
+UserSchema.pre("save",async function(next){
+    if(!this.isModified("password")){
+     var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync("B4c0/\/", salt);
+    this.password=hash;
+    next()
+}})
+
+UserSchema.methods.ispasswordcorrect=async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+UserSchema.methods.generateaccesstoken=async function(){
+    return jwt.sign({
+        name:this.name,
+        email:this.email
+    },
+process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+})
+}
+const User=mongoose.model('User',UserSchema)
+export{
+    User
+}
--- FrontEnd/src/pages/AutoService.jsx
+++ FrontEnd/src/pages/AutoService.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const AutoService = () => {
   return (
-  
+
     <div className="container mx-auto">
       <h1 className="text-2xl font-bold">Auto Page</h1>
     </div>
