9eebc79e3b4bbcfb25d9fd97325accabd01d5461
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-12-07 14:51:14 +0530

    Describe your changes here

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { loginUser, registerUser,logoutUser } from "../controllers/user.controller.js"
+import { loginUser, registerUser,logoutUser, forgotPassword } from "../controllers/user.controller.js"
 
 const router=Router()
 
@@ -7,4 +7,6 @@ const router=Router()
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 router.route("/logout").post(logoutUser)
+router.route("/forgotPassword").put(forgotPassword)
+
 export default router
