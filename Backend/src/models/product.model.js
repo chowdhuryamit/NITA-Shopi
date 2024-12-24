32fecb2ba3c1588ab535180fd9007de04b221619
chowdhuryamit <camit8546@gmail.com>
2024-12-24 23:43:23 +0530

    write product model

---
+++ Backend/src/models/product.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    quantity:{
+        type:Number,
+        required:true,
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+productSchema.plugin(mongooseAggregatePaginate);
+
+const Product=mongoose.model("Product",productSchema);
+export{Product};
---
+++ Backend/src/routes/product.routes.js
@@ -0,0 +1,6 @@
+import { Product } from "../models/product.model";
+import mongoose from "mongoose";
+
+const addProduct=async(req,res)=>{
+    const {name,description,}=req.body;
+}
