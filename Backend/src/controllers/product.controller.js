f4fb56ee065090880784488ba4a30e49e73c4cad
chowdhuryamit <camit8546@gmail.com>
2025-01-03 12:15:29 +0530

    complete to write all product controller

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -121,7 +121,7 @@ const getUserProduct = async (req, res) => {
 };
 
 const deleteProduct = async (req, res) => {
- 
+   try {
     const productId = req.query.id;
     
     if (!productId) {
@@ -172,8 +172,193 @@ const deleteProduct = async (req, res) => {
               "error occured while deleting product image from cloudinary",
           });
       });
+   } catch (error) {
+      return res.status(400).json({message:"some error occured while deleting product from database"});
+   }
 };
 
+const updateProductInfo=async(req,res)=>{
+    const productId=req.query.id;
+    if (!productId) {
+      return res.status(401).json({message:"productId is required"});
+    }
 
+    if(!req.user._id){
+      return res.status(401).json({message:"you are not logged in"});
+    }
 
-export { addProduct, getUserProduct, deleteProduct };
+    let product=await Product.findById(productId);
+
+    if(!product){
+      return res.status(404).json({message:"product not found"});
+    }
+    if(!product.owner.equals(req.user._id)){
+      return res.status(401).json({message:"you are not the owner of the product."});
+    }
+
+    const {name,description,quantity,price,}=req.body;
+
+    if(name){
+      product.name=name;
+    }
+    if(description){
+      product.description=description;
+    }
+    if(quantity){
+      product.quantity=quantity;
+    }
+    if(price){
+      product.price=price;
+    }
+
+    await product.save({validateBeforeSave:false});
+
+    return res.status(200).json({message:"product information updated successfully.",product});
+     
+}
+
+const updateProductImage=async(req,res)=>{
+   const productId=req.query.id;
+   if (!productId) {
+    return res.status(400).json({message:"product id is required"});
+   }
+   if(!req.user._id){
+    return res.status(400).json({message:"you are not logged in"});
+   }
+
+   const product=await Product.findById(productId);
+
+   if(!product){
+    return res.status(404).json({message:"product not found"});
+   }
+   if(!product.owner.equals(req.user._id)){
+    return res.status(401).json({message:"you are not the owner of the product."});
+   }
+
+   const productImageLocalPath=req.file?.path;
+   
+
+   if (!productImageLocalPath) {
+    return res.status(401).json({message:"upload product image."});
+   }
+
+   const productImageUrl=product.image;
+
+  let lastIndexBackslash = productImageUrl.lastIndexOf("/");
+  let lastIndexDot = productImageUrl.lastIndexOf(".");
+
+    const productImagePublic_id = productImageUrl.substring(
+      lastIndexBackslash + 1,
+      lastIndexDot
+    );
+
+    await cloudinary.uploader.destroy(productImagePublic_id,{resource_type:'image'})
+    .then(async()=>{
+      const response=await uploadOnCloudinary(productImageLocalPath);
+      if(!response){
+        return res.status(401).json({message:"error occured while uploading product image."});
+      }
+      else{
+        product.image=response?.secure_url;
+        await product.save({validateBeforeSave:false});
+        return res.status(200).json({message:"product image updated successfully.",product});
+      }
+    })
+    .catch((err)=>{
+      fs.unlinkSync(productImageLocalPath);
+      return res.status(401).json({message:"error occured while deleting product image."});
+    })
+}
+
+const viewProduct=async(req,res)=>{
+  const productId=req.query.id;
+  if(!productId){
+    return res.status(401).json({message:"product id is required."});
+  }
+  if(!req.user._id){
+    return res.status(400).json({message:"user is not logged in"});
+  }
+
+  const product=await Product.findById(productId);
+  if(!product){
+    return res.status(401).json({message:"product not found."});
+  }
+
+  const productDetails=await Product.aggregate([
+    {
+      $match:{
+        _id:new mongoose.Types.ObjectId(productId)
+      }
+    },
+    {
+      $lookup:{
+        from:"users",
+        localField:"owner",
+        foreignField:"_id",
+        as:"owner",
+        pipeline:[
+          {
+            $project:{
+              fullname:1,
+              email:1,
+              phonenumber:1,
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields:{
+        owner:{
+          $first:"$owner"
+        }
+      }
+    }
+  ])
+
+  return res.status(200).json({message:"product found",productDetails});
+}
+
+const getAllProduct=async(req,res)=>{
+    const { page = 1, limit = 1 } = req.query;
+    const parsedLimit = parseInt(limit, 10);
+    const pageSkip = (parseInt(page, 10) - 1) * parsedLimit;
+    const sortStage = {};
+    sortStage["updatedAt"] = "asc" ? 1 : -1;
+  
+    const products = await Product.aggregate([
+      {
+        $match: { quantity: { $gt: 0 } },
+      },
+      {
+        $project: {
+          name: 1,
+          price: 1,
+          image: 1,
+        },
+      },
+      {
+        $sort: sortStage,
+      },
+      {
+        $skip: pageSkip,
+      },
+      {
+        $limit: parsedLimit,
+      },
+    ]);
+
+    const totalDocuments = await Product.countDocuments({ quantity: { $gt: 0 } });
+    const totalPages = Math.ceil(totalDocuments / parsedLimit);
+
+    res.status(200).json({
+      message: "product fetched successfully",
+      currentPage: parseInt(page, 10),
+      totalPages,
+      totalItems: totalDocuments,
+      limit: parsedLimit,
+      data:products,
+    });
+}
+
+export { addProduct, getUserProduct, deleteProduct,updateProductInfo,updateProductImage,viewProduct,getAllProduct };
