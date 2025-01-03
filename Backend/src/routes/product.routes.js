1e22a92d886cd77f5d91f1c8dc317ffd5e3e7de4
chowdhuryamit <camit8546@gmail.com>
2025-01-03 12:16:18 +0530

    write product routes

--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addProduct, deleteProduct, getUserProduct } from "../controllers/product.controller.js"
+import { addProduct, deleteProduct, getAllProduct, getUserProduct, updateProductImage, updateProductInfo, viewProduct } from "../controllers/product.controller.js"
 
 const router=Router();
 
@@ -10,5 +10,9 @@ router.use(verifyJWT);
 router.post('/addProduct',upload.single("productImage"),addProduct);
 router.get('/getUserProduct',getUserProduct);
 router.delete('/deleteProduct',deleteProduct);
+router.patch('/updateProductInfo',updateProductInfo);
+router.patch('/updateProductImage',upload.single("productImage"),updateProductImage);
+router.get('/viewProduct',viewProduct);
+router.get('/getAllProduct',getAllProduct);
 
 export default router
